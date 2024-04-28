5cf3a416347c1c56fd852b6254088ed8af7f199f
bappowappo <pazdzior.krzysztof01@gmail.com>
2024-04-28 16:29:32 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace poprojekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("to jest projekt na po - naciśnij jakikolwiek klawisz by go zamknąć");
+            Console.ReadLine();
+
+        }
+    }
+}
